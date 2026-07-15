55115d36ecf088a4c0d8114333fcda04bdee5da1
talha.satir <talha.satir@mehaz.com.tr>
2026-07-15 16:22:58 +0300

    Add project files.

---
+++ Payment.Core/Event/PaymentCompletedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment.Core.Event;
+
+public record PaymentCompletedEvent
+{
+    public Guid PaymentId { get; init; }
+    public decimal Amount { get; init; }
+    public string Email { get; init; }
+    public DateTime CompletedAt { get; init; }
+}
